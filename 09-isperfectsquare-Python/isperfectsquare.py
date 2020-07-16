2f37f79ef46ca8ef261e6ff0021f834ccea5d422
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-16 23:25:35 +0530

    Jul 16  23:25:34

--- 09-isperfectsquare-Python/isperfectsquare.py
+++ 09-isperfectsquare-Python/isperfectsquare.py
@@ -7,9 +7,9 @@ def isperfectsquare(n):
 	# your code goes here
   r = math.sqrt(n)
   t = n
-  if (type(n) == int and n <= 0):
+  if (type(n) != int or (n <= 0)):
        return False
-  if ( n > 0):
+  if (type(n) == int and  (n > 0)):
     if (r ** 2 == t):
         return True
     else:
