fad4f9e7ec0bb30019946d1c2f3b4cfb94c809a2
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-17 01:05:50 +0530

    Jul 17  01:05:50

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,5 +7,16 @@
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
+    i = 0
+    a = abs(n)
+    d = str(a)
+    q = [int(i)  for i in list(d)]
+    for x in range(len(q)):
+        if x == k:
+            q[x] = d
+            st = [str(i) for i in str(q)]
+            s = "".join(st)
+            t = int(s)
+            return t
+  
 
