560928616e3211a49c1e76c73844f1f42010bd77
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-21 00:08:39 +0530

    Jul 21  00:08:39

--- 01-nth_happy_number-Python/nth_happy_number.py
+++ 01-nth_happy_number-Python/nth_happy_number.py
@@ -11,5 +11,32 @@
 # assert(nthHappyNumber(7) == 31)
 
 
-def fun_nth_happy_number(n):
-	return 0
+def fun_nth_happy_number(n): 
+    
+    l1 = []
+    l.append(0)
+    if n == 0:
+        return 1
+        for i in range(2,50):
+            if is_hap(i):
+                l.append(i)
+        return l[n]
+def is_hap(n):            
+    def sum_sq(n):
+      ss = 0
+      while (n > 0):
+        p += (n % 10)**2
+        n = int(n//10)
+      return ss  
+
+
+      l = []
+      while sum_sq(n) not in l:
+        res = sum_sq(n)
+        if res == 1:
+            return True
+        else:
+            l.append(res)
+            n = res
+      return False
+
