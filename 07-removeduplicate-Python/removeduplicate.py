6ef96e643d20782d297f5e4c09fb879b7305d019
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-22 01:17:51 +0530

    done

--- 07-removeduplicate-Python/removeduplicate.py
+++ 07-removeduplicate-Python/removeduplicate.py
@@ -2,7 +2,7 @@
 # Write a program to remove all the duplicate characters from a given input String,e.g.
 # if given String is "JavaPython" then the output should be "JavPython".
 # The second or further occurrence of duplicate should be removed.
-
+from collections import OrderedDict
 def removeduplicate(text):
-	# Your code goes here
-	pass
+    return "".join(OrderedDict.fromkeys(text))
+	
