9deb92a1fc4d7931b9a2d28c035fba1b41570cf4
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-21 23:41:19 +0530

    Jul 21  23:41:19

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -4,5 +4,33 @@
 
 
 
+def is_hap(n):            
+    def sum_sq(n):
+      ss = 0
+      if ss != 4:
+        while (n > 0):
+          r = n % 10
+          ss += r**2
+          n = int(n//10)  
+        return ss  
+    
+    l = []
+    while sum_sq(n) not in l:
+        res = sum_sq(n)
+        if res == 1:
+            return True
+        else:
+            l.append(res)
+            n = res
+    return False
+
+def is_prime(n):
+    if n > 1:
+        for x in range(2,n):
+            if (n % x == 0):
+                return False
+        return True
+
 def fun_nth_happy_prime(n):
-	return 0
+    l1 = [i for i in range(100) if (is_hap(i) and is_prime(i))]
+    return l1[n]
