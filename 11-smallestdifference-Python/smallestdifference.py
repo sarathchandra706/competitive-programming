0dd73f63a6d63697b38699c8369b3bfaf73bbe99
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-22 03:15:00 +0530

    done

--- 11-smallestdifference-Python/smallestdifference.py
+++ 11-smallestdifference-Python/smallestdifference.py
@@ -3,13 +3,13 @@
 # integers in the list. If the list is empty, return -1. For example:
 #       assert(smallestDifference([19,2,83,6,27]) == 4)
 # The two closest numbers in that list are 2 and 6, and their difference is 4.
-import math
 def smallestdifference(a):
   if len(a) == 0:
-        return -1
+    return -1
+    
   l = sorted(a)
   v =  l[1]-l[0]
   for x in range(len(l)-1):
-    if  l[x+1]-l[x] < v:
+    if  (l[x+1]-l[x]) < v:
         v =  l[x+1]-l[x]
-  return (x)
+  return v
