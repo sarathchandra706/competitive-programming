da47e4d6d274f39954590704425813394ffad0a3
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-24 22:48:41 +0530

    Jul 24  22:48:40

--- 03-hasduplicates-Python/hasduplicates.py
+++ 03-hasduplicates-Python/hasduplicates.py
@@ -4,5 +4,17 @@
 # if any two values in L are equal to each other), and False otherwise.
 
 def hasduplicates(L):
-	# Your code goes here
-	pass
+  c = 0
+  li = []
+  l = len(L)
+  for i in range(l):
+      for j in range(l[i]):
+        if L[i][j] in li:
+          c+=1
+        else:
+          li.append(L[i][j])
+      
+  if c >= 1:
+     return True
+  else:
+     return False
