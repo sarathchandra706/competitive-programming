c70f3ca745901a3e4c68259d19c2255d2c7691ee
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-27 01:05:58 +0530

    done

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -5,5 +5,25 @@
 # because there is a run of 3 consecutive 7's), 
 # as does longestDigitRun(-677886).
 def longestdigitrun(n):
-	# Your code goes here
-	pass
+  if n <0:
+      n = -n
+  l = list(map(int,str(n)))
+  dict = {}
+  c = 1
+  for i in range(len(l)-1):
+      if l[i] == l[i+1]:
+          c = 0
+          if l[i] in dict:
+              dict[l[i]] = dict[l[i]]+1
+          else:
+              dict[l[i]] = 1
+              
+  if c == 1:
+      l.sort()
+      return l[0]
+  dic = {}
+  f = sorted(dict.keys())
+  for i in f:
+      dic[i] = dict[i]
+  dic = sorted(dic.items(),key = lambda item:item[1], reverse = True)
+  return dic[0][0]
