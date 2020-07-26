528fba4bc1c14b0cbeab1bf659a9b565c9ff25dc
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-27 00:46:24 +0530

    Jul 27  00:46:24

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,6 +7,50 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
-
+import math
 def fun_nth_smithnumber(n):
-    return 1
+  l =[]
+  for i in range(1,500):
+    if issmith(i) and not iscomp(i):
+        l.append(i)
+  return l[n-1]
+
+def iscomp(n):
+  for i in range (2,n):
+      if (n% 1 == 0 and n% i == 0):
+          return False
+  return True
+
+def sumofdig(n):
+    s = 0
+    while(n >0):
+        r = n%10
+        s = s + r
+        n = n//10
+    return s
+
+def sumfact(n):
+    l = []
+    while n%2 == 0 and n>0:
+        l.append(int(2))
+        n = n/2
+    for i in range(3,int(math.sqrt(n))+1,2):
+        while n%i == 0:
+            l.append(int(i))
+            n = n/i
+        if n >2:
+            l.append(int(n))
+        s = 0
+        for i in l:
+            if len(str(i))==1:
+                s = s+i
+            elif len(str(i)) > 1 and i is not n :
+                s = s + sumofdig(i)
+        return s
+    
+def issmith(n):
+    if sumofdig(n)== sumfact(n):
+        return True
+    else:
+        return False
+                
