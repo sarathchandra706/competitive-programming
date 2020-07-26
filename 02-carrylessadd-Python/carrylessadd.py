7bb8a7b98a1ff3517e494466413fab748f5dcacf
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-27 00:00:10 +0530

    Jul 27  00:00:09

--- 02-carrylessadd-Python/carrylessadd.py
+++ 02-carrylessadd-Python/carrylessadd.py
@@ -6,5 +6,22 @@
 
 
 def fun_carrylessadd(x, y):
-	return 0
+  s = 0
+  if (x == 0):
+      return y
+  if (y == 0):
+      return x
+  i = 1
+  sum = 0
+  while(x or y):
+      sum = ((x%10) +(y%10))
+      sum = sum%10
+      s = (sum*i)+s
+      x = math.floor(x/10)  
+      y = math.floor(y/10) 
+      i = i*10
+  st = str(s)
+  st.strip("0")
+  return int(s)
+
 
