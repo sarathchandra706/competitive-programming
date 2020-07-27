5f3c7f04b07ccd44b72acc6dfadd917cf02fab71
sarathchandra706 <sarathchandra706@gmail.com>
2020-07-27 22:54:41 +0530

    Jul 27  22:54:41

--- 06-leastfrequentletters-Python/leastfrequentletters.py
+++ 06-leastfrequentletters-Python/leastfrequentletters.py
@@ -6,7 +6,21 @@
 # returns "be". Note that digits, punctuation, and whitespace are not letters! Also note that seeing as we have not 
 # yet covered lists, sets, maps, or efficiency, you are not expected to write the most efficient solution. Finally, 
 # if s does not contain any alphabetic characters, the result should be the empty string ("")
-
+import string
 def leastfrequentletters(s):
-	# Your code goes here
-	pass
+  x = 0
+  min = 1000
+  r = ''
+  s = s.lower()
+  let = String.ascii_lowercase
+  while x < 26:
+      c = s.count(let[x])
+      if min >c>0:
+          min = c
+          r = let[x]
+      elif min == c:
+          r+=let[x]
+      x+=1
+  if r == '':
+    return r
+  return r
